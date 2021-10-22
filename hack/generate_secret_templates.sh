c1ee094ed2615a4fc96d743f21d1958dd1e0120a
gi8 <giottolino@gmail.com>
2021-10-22 16:31:36 +0200

    feat(generate_secret_templates): optimize code

--- hack/generate_secret_templates.sh
+++ hack/generate_secret_templates.sh
@@ -2,27 +2,65 @@
 
 set -o errexit
 
-[[ ! "${PWD}" =~ "k8s-gitops/hack" ]] && echo "please cd to the 'hack' folder and run the script from there!" && exit 1
+[[ ! "${PWD}" =~ "k8s-gitops/hack" ]] && \
+  echo "please cd to the 'hack' folder and run the script from there!" && \
+  exit 1
 
-secrets=$(grep --files-with-matches --recursive --regexp "^kind: Secret$" --exclude="*.template" ..)
-SECRET_WARN_MESSAGE="##################################################
-# make sure to delete all secrets before saving! #
-##################################################"
+ShowHelp() {
+    printf "
+Usage: generate_secret_template.sh [-f|--force]
+                                   | [-h|--help]
+
+Generates 'secret.template' files for SOPS-encrypted Kubernetes secrets.
+Unencrypted secret will be automatically decrypted!
+
+-f, --force                         override existing templates
+-h, --help                          display this help and exit
+\n"
+    exit 0
+}
+
+while [ $# -gt 0 ]; do
+    key="$1"
+    case $key in
+        -f|--force)
+        FORCE=True
+        shift
+        ;;
+        -h|--help)
+        ShowHelp
+        ;;
+        *)  # unknown option
+        printf "%s\n" \
+           "$(basename $BASH_SOURCE): invalid option -- '$1'" \
+           "Try '$(basename $BASH_SOURCE) --help' for more information."
+        exit 1
+        ;;
+    esac
+done
+
+secrets=$(grep \
+              --files-with-matches \
+              --recursive \
+              --regexp "^kind: Secret$" \
+              --exclude="*.template" ..)
 
 for secret in ${secrets}; do
   filepath=$(dirname "${secret}")
   filename=$(basename "${secret}")
   secret_template_name="${filepath}/${filename%.*}.template"
 
-  [[ -f ${secret_template_name} ]] && echo "secret template '${secret_template_name}' already exists" && continue
+  [ "${FORCE}" = True ] && \
+    rm -f ${secret_template_name}
 
-  echo "creating secret template '${secret_template_name}'"
+  [[ -f ${secret_template_name} ]] && \
+    echo "secret template '${secret_template_name}' already exists" && \
+    continue
 
+  # encrypt secret if necessary
   grep --quiet --regexp "ENC.AES256" "${secret}" || sops --encrypt --in-place "${secret}" 2> /dev/null
 
-  printf '%s\n' "${SECRET_WARN_MESSAGE}" "$(sops --decrypt "${secret}")" > "${secret_template_name}"
+  echo "creating secret template '${secret_template_name}'"
+  printf '%s\n' > ${secret_template_name} "$(yq eval '.stringData[] = ""' <(sops --decrypt ${secret}))"
 
-  vim "${secret_template_name}"
-  tail -n +4 "${secret_template_name}" > "${secret_template_name}2"
-  mv "${secret_template_name}2" "${secret_template_name}"
 done
