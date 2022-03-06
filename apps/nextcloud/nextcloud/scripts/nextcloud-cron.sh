98841a4f0e790e289dcd3087fa13949fe33d895a
gi8 <giottolino@gmail.com>
2022-03-06 16:23:23 +0100

    feat(nextcloud): add script for cron

---
+++ apps/nextcloud/nextcloud/scripts/nextcloud-cron.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+HEALTHCHECKS_URL=${HEALTHCHECKS_URL%*/}
+NEXTCLOUD_CRON_URL="${NEXTCLOUD_CRON_URL:-http://nextcloud.nextcloud.svc.cluster.local:8000/cron.php)"
+
+[ -z "${HEALTHCHECKS_URL}" ] && \
+  echo "'HEALTHCHECKS_URL' is not set!" && \
+  exit 1
+
+[ -z "${NEXTCLOUD_DOMAIN}" ] && \
+  echo "'NEXTCLOUD_DOMAIN' is not set!" && \
+  exit 1
+
+response=$(curl \
+            --fail \
+            --location \
+            --write-out "%{http_code}") \
+            --output /dev/null \
+            --header Host: ${NEXTCLOUD_DOMAIN} \
+            "${NEXTCLOUD_CRON_URL}"
+
+[ "${response}" != "200" ] && \
+  URL="${HEALTHCHECKS_URL}/fail"
+
+curl \
+  --fail \
+  --silent \
+  --show-error \
+  --max-time 10 \
+  --retry 5 \
+  --output /dev/null \
+  "${HEALTHCHECKS_URL}"
