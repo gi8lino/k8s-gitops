d2359e1848540f0628f8b301af1062a6b2d166af
gi8 <giottolino@gmail.com>
2022-03-06 17:39:01 +0100

    feat(nextcloud): add description for cron script

--- apps/nextcloud/nextcloud/scripts/nextcloud-cron.sh
+++ apps/nextcloud/nextcloud/scripts/nextcloud-cron.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# execute a curl command to trigger nextcloud cron.php and afterwards ping healthchecks
+# to update cronjob status
+# nextcloud cron.php description:
+# https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/background_jobs_configuration.html#cron-jobs
+
 set -o errexit
 
 HEALTHCHECKS_URL=${HEALTHCHECKS_URL%*/}
@@ -25,10 +30,12 @@ response=$(curl \
   HEALTHCHECKS_URL="${HEALTHCHECKS_URL}/fail"
 
 curl \
-  --fail \
-  --silent \
-  --show-error \
+  -fsS \
   --max-time 10 \
   --retry 5 \
   --output /dev/null \
   "${HEALTHCHECKS_URL}"
+
+# -f: Makes curl treat non-200 responses as errors.
+# -s: Silent or quiet mode. Hides the progress meter, but also hides error messages.
+# -S: Re-enables error messages when -s is used.
