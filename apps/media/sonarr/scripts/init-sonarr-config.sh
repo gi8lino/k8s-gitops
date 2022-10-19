b95a75652b5bc6cabea2e1748aca92cb545fff25
gi8 <giottolino@gmail.com>
2022-10-19 13:58:45 +0200

    feat(sonarr): 30-config

---
+++ apps/media/sonarr/scripts/init-sonarr-config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bash
+
+# cleanup pid if it exists
+[[ -e /config/sonarr.pid ]] && \
+    rm -rf /config/sonarr.pid
+
+# because an nfs share is mounted to '/config/backups' and there are problems with permissions,
+# set ownership to all other files & directories recursively
+# '-not -path /config' is ignored to prevent error when setting ownership to '/config' recursively
+find /config \
+    -maxdepth 1 \
+    -not -path /config \
+    -not -path /config/Backups \
+    -exec chown -R abc:abc {} +
