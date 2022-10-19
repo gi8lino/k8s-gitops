bb32b108c44526a3f16c8ab622a5d8813d3ffc4d
gi8 <giottolino@gmail.com>
2022-10-19 13:43:55 +0200

    fix(radarr): init script

---
+++ apps/media/radarr/scripts/init-radarr-config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/with-contenv bash
+
+# because an nfs share is mounted to '/config/backups' and there are problems with permissions,
+# set ownership to all other files & directories recursively
+# '-not -path /config' is ignored to prevent error when setting ownership to '/config' recursively
+find /config \
+    -maxdepth 1 \
+    -not -path /config \
+    -not -path /config/Backups \
+    -exec chown -R abc:abc {} +
