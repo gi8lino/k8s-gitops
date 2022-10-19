357440f739310c03d6fd470e0b98bda542b98e68
gi8 <giottolino@gmail.com>
2022-10-19 14:01:58 +0200

    feat(radarr): make script similar to sonarr

--- apps/media/radarr/scripts/init-radarr-config.sh
+++ apps/media/radarr/scripts/init-radarr-config.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
+# cleanup pid if it exists
+[[ -e /config/radarr.pid ]] && \
+    rm -rf /config/radarr.pid
+
 # because an nfs share is mounted to '/config/backups' and there are problems with permissions,
 # set ownership to all other files & directories recursively
 # '-not -path /config' is ignored to prevent error when setting ownership to '/config' recursively
