b2fcddcbc53b0a7ba1f56b9274d5e6cb90b6d4cc
gi8 <giottolino@gmail.com>
2022-10-19 13:27:00 +0200

    fix(tautulli): start script

--- apps/media/tautulli/scripts/init-tautulli-config.sh
+++ apps/media/tautulli/scripts/init-tautulli-config.sh
@@ -4,7 +4,11 @@
 chown -R abc:abc \
     /app
 
+# because an nfs share is mounted to '/config/backups' and there are problems with permissions,
+# set ownership to all other files & directories recursively
+# '-not -path /config' is ignored to prevent error when setting ownership to '/config' recursively
 find /config \
     -maxdepth 1 \
+    -not -path /config \
     -not -path /config/backups \
     -exec chown -R abc:abc {} +
