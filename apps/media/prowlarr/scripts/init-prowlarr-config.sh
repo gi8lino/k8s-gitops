3544f5d4fca157ebeb26c8af802b494178f1438a
gi8 <giottolino@gmail.com>
2022-10-20 11:04:55 +0200

    fix(init-scripts): optimize find command

--- apps/media/prowlarr/scripts/init-prowlarr-config.sh
+++ apps/media/prowlarr/scripts/init-prowlarr-config.sh
@@ -3,9 +3,8 @@
 
 # because an nfs share is mounted to '/config/backups' and there are problems with permissions,
 # set ownership to all other files & directories recursively
-# '-not -path /config' is ignored to prevent error when setting ownership to '/config' recursively
 find /config \
+    -mindepth 1 \
     -maxdepth 1 \
-    -not -path /config \
     -not -path /config/Backups \
     -exec chown -R abc:abc {} +
--- apps/media/tautulli/scripts/init-tautulli-config.sh
+++ apps/media/tautulli/scripts/init-tautulli-config.sh
@@ -5,10 +5,10 @@
 chown -R abc:abc \
     /app
 
-# because an nfs share is mounted to '/config/backups' and there are problems with permissions,
+# because a nfs share is mounted to '/config/backups' and there are problems with permissions,
 # set ownership to all other files & directories recursively
-# '-not -path /config' is ignored to prevent error when setting ownership to '/config' recursively
 find /config \
+    -mindepth 1 \
     -maxdepth 1 \
     -not -path /config \
     -not -path /config/backups \
